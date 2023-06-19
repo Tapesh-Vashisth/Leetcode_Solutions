0b035add6d0e830f12b4304be71309878114ad46
tapesh <tapeshvashisth789@gmail.com>
2023-06-19 16:06:19 +0530

    painting the walls

--- 6447_Painting_the_Walls.cpp
+++ 6447_Painting_the_Walls.cpp
@@ -4,39 +4,39 @@
 #include <map>
 using namespace std;
 
-map <string, int> dp;
-    
-int helper(vector <int> & cost, vector <int> & time, string & visited, int counter, int total) {
-    if (total >= cost.size() - counter) {
+vector <vector <long long>> dp;
+
+long long helper(vector <int> & cost, vector <int> & time, int index, int counterplustotal) {
+    if (counterplustotal >= cost.size()) {
         return 0;
     }
-    
-    if (dp[visited] != 0) {
-        return dp[visited];
-    }
 
-    int hold = INT32_MAX;
-
-    for (int i = 0; i < cost.size(); i++) {
-        if (visited[i] == '0') {
-            visited[i] = '1';
-            hold = min(hold, helper(cost, time, visited, counter + 1, total + time[i]) + cost[i]);
-            visited[i] = '0';
-        }
+    if (index >= cost.size()) {
+        return INT32_MAX;
     }
     
-    dp[visited] = hold;
+    if (dp[index][counterplustotal] != -1) {
+        return dp[index][counterplustotal];
+    }
+
+    long long hold = INT32_MAX;
+
+    // take     
+    long long take = helper(cost, time, index + 1, counterplustotal + time[index] + 1) + cost[index];
+
+    // not take 
+    long long notTake = helper(cost, time, index + 1, counterplustotal);
+
+    hold = min(take, notTake);
+    
+    dp[index][counterplustotal] = hold;
 
     return hold;
 }
 
 int paintWalls(vector<int>& cost, vector<int>& time) {
-    string visited = "";
-    for (auto it: cost) {
-        visited.push_back('0');
-    }
-    int ans = helper(cost, time, visited, 0, 0);
-    
+    dp = vector <vector <long long>> (cost.size(), vector <long long> (cost.size(), -1));
+    long long ans = helper(cost, time, 0, 0);
     return ans;
 }
 
