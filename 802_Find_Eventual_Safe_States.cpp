617b42165741d94f222a9419aa3853c9cfbfa809
tapesh <tapeshvashisth789@gmail.com>
2023-07-12 10:36:40 +0530

    daily

--- 802_Find_Eventual_Safe_States.cpp
+++ 802_Find_Eventual_Safe_States.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <algorithm>
 using namespace std;
 
 bool helper(vector <vector <int>> & graph, int root, vector <int> & hash) {
@@ -38,6 +40,41 @@ vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
             ans.push_back(i);
         } 
     }
+
+    return ans;
+}
+
+vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+    vector <vector <int>> newGraph(graph.size());
+    vector <int> indegrees(graph.size(), 0);
+
+    for (int i = 0; i < graph.size(); i++) {
+        for (auto it: graph[i]) {
+            indegrees[i]++;
+            newGraph[it].push_back(i);
+        }
+    }        
+
+    vector <int> ans;
+    set <pair <int, int>> store;
+    for (int i = 0; i < indegrees.size(); i++) {
+        store.insert({indegrees[i], i});
+    }
+
+
+    while (store.size() > 0 && (*store.begin()).first == 0) {
+        pair <int, int> hold = (*store.begin());
+        store.erase(store.begin());
+        ans.push_back(hold.second);
+
+        for (auto it: newGraph[hold.second]) {
+            store.erase({indegrees[it], it});
+            indegrees[it]--;
+            store.insert({indegrees[it], it});
+        }
+    }
+
+    sort(ans.begin(), ans.end());
 
     return ans;
 }
