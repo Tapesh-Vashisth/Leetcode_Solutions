ac4d718dca9a2e241bbe6871e00128f91e567ce2
tapesh <tapeshvashisth789@gmail.com>
2023-07-25 10:36:03 +0530

    daily

---
+++ 162_Find_Peak_Element.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findPeakElement(vector<int>& arr) {
+    int low = 0;
+    int high = arr.size() - 1;
+    int ans = 0;
+    while (low < high) {
+        int mid1 = low + (high - low)/3;
+        int mid2 = low + (2*(high - low))/3;
+
+        if (arr[mid1] > arr[ans]) {
+            ans = mid1;
+        }
+
+        if (arr[mid2] > arr[ans]) {
+            ans = mid2;
+        }
+
+        if (arr[low] > arr[ans]) {
+            ans = low;
+        }
+
+        if (arr[high] > arr[ans]) {
+            ans = high;
+        }
+
+        if (arr[mid1] == arr[mid2]) {
+            low = mid1 + 1;
+            high = mid2;
+        } else if (arr[mid1] > arr[mid2]) {
+            high = mid2 - 1;
+        } else {
+            low = mid1 + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    return 0;
+}
